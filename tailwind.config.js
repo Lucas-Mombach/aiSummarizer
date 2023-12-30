e24ec003effe81c529f3aac97a3bcf6d3ed89202
Lucas Mombach <lucasmmombach@gmail.com>
2023-12-30 11:36:30 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi","sans-serif"],
+        inter: ["Inter","sans-serif"],
+
+      }
+    },
+  },
+  plugins: [],
+}
+
