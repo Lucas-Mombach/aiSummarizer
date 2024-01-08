f4d25d710ef61d5c2213be2e7fa330835a3e61f0
Lucas Mombach <lucasmmombach@gmail.com>
2024-01-08 10:42:41 -0300

    add services

---
+++ src/services/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { articleApi } from "./article";
+
+export const store = configureStore({
+  reducer: {
+    [articleApi.reducePath]:articleApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
+})
