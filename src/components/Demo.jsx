3b4fb82f768afcd06bb42a25cde49bd671a4e4cf
Lucas Mombach <lucasmmombach@gmail.com>
2024-01-08 10:14:10 -0300

    add submitty logic

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,12 +3,21 @@ import {copy, linkIcon,loader,tick} from '../assets';
 
 
 const Demo = () => {
+  const[article,setArticle] = useState({
+    url:'',
+    summary: '',
+
+  });
+  
+  const handleSubmit = async(e) => {
+    alert('Submitted');
+  }
   return (
     <section className="mt-16 w-full max-w-x1">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
-          className='relative flex justify-center items-center' onSubmit={() => {}}>
+          className='relative flex justify-center items-center' onSubmit={handleSubmit}>
             <img 
               src={linkIcon}
               alt='link_icon'
@@ -17,8 +26,8 @@ const Demo = () => {
             <input
             type='url'
             placeholder='Enter a URL'
-            value=" "
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({article,url:e.target.value})}
             required
             className='url_input peer' 
             />
@@ -26,8 +35,9 @@ const Demo = () => {
               type="submit"
               className='submit_btn peer-focus:border-gray-700
               peer-focus:text-gray-700'
-            >↵</button>
-          </form>
+              >↵</button>
+            </form>
+        
 
       </div>
     </section>
